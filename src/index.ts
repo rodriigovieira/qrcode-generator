6a302cfb33a81941bc391fb2f34201ec7fecba57
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-01-31 15:25:49 -0300

    setting up typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-import { flow, flattenDeep, uniq, orderBy } from 'lodash';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
-const list = [1, 2, 3, 2, [5, [4, 1]]];
+import AppError from './errors/AppError';
 
-export const flattenUniqOrderBy = flow(
-  flattenDeep,
-  uniq,
-  orderBy,
-);
+import createConnection from "./database";
 
-console.log(flattenUniqOrderBy(list));
+createConnection();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send();
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
